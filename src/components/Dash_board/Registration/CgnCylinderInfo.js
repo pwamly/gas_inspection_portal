cf6d2f37e507b14a5c8969815f98c6b7a821e3df
stephano wamlyoma <stephano@bongolive.co.tz>
2021-03-04 22:47:44 +0300

    --amend

--- src/components/Dash_board/Registration/CgnCylinderInfo.js
+++ src/components/Dash_board/Registration/CgnCylinderInfo.js
@@ -5,10 +5,15 @@ import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 
 const style = {
-  maxWidth: 300,
+  display: "flex",
+  flexDirection: "row",
+};
+const stylemain = {
   padding: "20px",
   borderRadius: "16px",
   margin: "auto",
+
+  width: "800px",
   transition: "0.3s",
   boxShadow: "0 8px 40px -12px rgba(0,0,0,0.4)",
   "&:hover": {
@@ -18,11 +23,21 @@ const style = {
 
 function CgnCylinderInfo({ formData, setForm, navigation }) {
   const {
-    cylinderno,
-    cylinderposition,
-    cylindertype,
-    cmanufacturer,
-    cmanuContact,
+    cylinderno1,
+    cylinderposition1,
+    cylindertype1,
+    cmanufacturer1,
+    cmanuContact1,
+    cylinderno2,
+    cylinderposition2,
+    cylindertype2,
+    cmanufacturer2,
+    cmanuContact2,
+    cylinderno3,
+    cylinderposition3,
+    cylindertype3,
+    cmanufacturer3,
+    cmanuContact3,
   } = formData;
 
   async function handle() {
@@ -33,59 +48,166 @@ function CgnCylinderInfo({ formData, setForm, navigation }) {
   }
 
   return (
-    <Container maxWidth="xs">
-      <Card variant="outlined" style={style}>
+    <Container>
+      <Card style={stylemain}>
         <h3> CNG CYLINDERS INFORMATION </h3>{" "}
-        <TextField
-          label="Cylinder No."
-          value={cylinderno}
-          onChange={setForm}
-          name="cylinderno"
-          margin="normal"
-          variant="outlined"
-          autoComplete="off"
-          fullWidth
-        />
-        <TextField
-          label="Cylinder position on the vehicle"
-          value={cylinderposition}
-          onChange={setForm}
-          name="cylinderposition"
-          margin="normal"
-          variant="outlined"
-          autoComplete="off"
-          fullWidth
-        />
-        <TextField
-          label="Cylinder Type"
-          value={cylindertype}
-          onChange={setForm}
-          name="cylindertype"
-          margin="normal"
-          variant="outlined"
-          autoComplete="off"
-          fullWidth
-        />
-        <TextField
-          label="Manufacturer Name"
-          value={cmanufacturer}
-          onChange={setForm}
-          name="cmanufacturer"
-          margin="normal"
-          variant="outlined"
-          autoComplete="off"
-          fullWidth
-        />
-        <TextField
-          label="Manufacturer Contact"
-          value={cmanuContact}
-          onChange={setForm}
-          name="cmanuContact"
-          margin="normal"
-          variant="outlined"
-          autoComplete="off"
-          fullWidth
-        />
+        <Card fullWidth style={style}>
+          <Card>
+            <TextField
+              label="Cylinder No."
+              value={cylinderno1}
+              onChange={setForm}
+              name="cylinderno1"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+              maxWidth=""
+            />
+            <TextField
+              label="Cylinder position on vehicle"
+              value={cylinderposition1}
+              onChange={setForm}
+              name="cylinderposition1"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+              maxWidth=""
+            />
+            <TextField
+              label="Cylinder Type"
+              value={cylindertype1}
+              onChange={setForm}
+              name="cylindertype1"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+              maxWidth=""
+            />
+            <TextField
+              label="Manufacturer Name"
+              value={cmanufacturer1}
+              onChange={setForm}
+              name="cmanufacturer1"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+              maxWidth=""
+            />
+            <TextField
+              label="Manufacturer Contact"
+              value={cmanuContact1}
+              onChange={setForm}
+              name="cmanuContact1"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+              maxWidth=""
+            />
+          </Card>{" "}
+          <Card>
+            <TextField
+              label="Cylinder No."
+              value={cylinderno2}
+              onChange={setForm}
+              name="cylinderno2"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+            />
+            <TextField
+              label="Cylinder position on vehicle"
+              value={cylinderposition2}
+              onChange={setForm}
+              name="cylinderposition2"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+              maxWidth=""
+            />
+            <TextField
+              label="Cylinder Type"
+              value={cylindertype2}
+              onChange={setForm}
+              name="cylindertype2"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+              maxWidth=""
+            />
+            <TextField
+              label="Manufacturer Name"
+              value={cmanufacturer2}
+              onChange={setForm}
+              name="cmanufacturer2"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+              maxWidth=""
+            />
+            <TextField
+              label="Manufacturer Contact"
+              value={cmanuContact2}
+              onChange={setForm}
+              name="cmanuContact2"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+              maxWidth=""
+            />
+          </Card>{" "}
+          <Card>
+            <TextField
+              label="Cylinder No."
+              value={cylinderno3}
+              onChange={setForm}
+              name="cylinderno3"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+              maxWidth=""
+            />
+            <TextField
+              label="Cylinder position on vehicle"
+              value={cylinderposition3}
+              onChange={setForm}
+              name="cylinderposition3"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+              maxWidth=""
+            />
+            <TextField
+              label="Cylinder Type"
+              value={cylindertype3}
+              onChange={setForm}
+              name="cylindertype3"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+              maxWidth=""
+            />
+            <TextField
+              label="Manufacturer Name"
+              value={cmanufacturer3}
+              onChange={setForm}
+              name="cmanufacturer3"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+              maxWidth=""
+            />
+            <TextField
+              label="Manufacturer Contact"
+              value={cmanuContact3}
+              onChange={setForm}
+              name="cmanuContact3"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+              maxWidth=""
+            />
+          </Card>{" "}
+        </Card>
         <div style={{ marginTop: "1rem" }}>
           {" "}
           <Button
@@ -105,7 +227,7 @@ function CgnCylinderInfo({ formData, setForm, navigation }) {
             Next{" "}
           </Button>{" "}
         </div>{" "}
-      </Card>{" "}
+      </Card>
     </Container>
   );
 }
--- src/components/Dash_board/Registration/CgnCylinderInfoCont.js
+++ src/components/Dash_board/Registration/CgnCylinderInfoCont.js
@@ -5,10 +5,15 @@ import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
 
 const style = {
-  maxWidth: 300,
+  display: "flex",
+  flexDirection: "row",
+};
+const stylemain = {
   padding: "20px",
   borderRadius: "16px",
   margin: "auto",
+
+  width: "800px",
   transition: "0.3s",
   boxShadow: "0 8px 40px -12px rgba(0,0,0,0.4)",
   "&:hover": {
@@ -18,11 +23,21 @@ const style = {
 
 function CgnCylinderInfoCont({ formData, setForm, navigation }) {
   const {
-    servicepressure,
-    cmanufacturedDate,
-    waterVolume,
-    expiryDate,
-    tbscertificate,
+    servicepressure1,
+    cmanufacturedDate1,
+    waterVolume1,
+    expiryDate1,
+    tbscertificate1,
+    servicepressure2,
+    cmanufacturedDate2,
+    waterVolume2,
+    expiryDate2,
+    tbscertificate2,
+    servicepressure3,
+    cmanufacturedDate3,
+    waterVolume3,
+    expiryDate3,
+    tbscertificate3,
   } = formData;
 
   async function handle() {
@@ -33,63 +48,157 @@ function CgnCylinderInfoCont({ formData, setForm, navigation }) {
   }
 
   return (
-    <Container maxWidth="xs">
-      <Card variant="outlined" style={style}>
-        <h3> Continuation... </h3>{" "}
-        <TextField
-          label="Service Pressure(MPa)"
-          value={servicepressure}
-          onChange={setForm}
-          name="  servicepressure,
-    cmanufacturedDate,
-    waterVolume,
-    expiryDate,
-    tbscertificate,"
-          margin="normal"
-          variant="outlined"
-          autoComplete="off"
-          fullWidth
-        />
-        <TextField
-          label="Manufactured Date"
-          value={cmanufacturedDate}
-          onChange={setForm}
-          name="cmanufacturedDate"
-          margin="normal"
-          variant="outlined"
-          autoComplete="off"
-          fullWidth
-        />
-        <TextField
-          label="Water volume(litres)"
-          value={waterVolume}
-          onChange={setForm}
-          name="waterVolume"
-          margin="normal"
-          variant="outlined"
-          autoComplete="off"
-          fullWidth
-        />
-        <TextField
-          label="Expiry Date"
-          value={expiryDate}
-          onChange={setForm}
-          name="expiryDate"
-          margin="normal"
-          variant="outlined"
-          autoComplete="off"
-          fullWidth
-        />
-        <TextField
-          label="TBS certificare"
-          value={tbscertificate}
-          onChange={setForm}
-          name="tbscertificate"
-          margin="normal"
-          variant="outlined"
-          autoComplete="off"
-          fullWidth
-        />
+    <Container>
+      <Card style={stylemain}>
+        <h3> CNG CYLINDERS INFORMATION Cont ... </h3>{" "}
+        <Card fullWidth style={style}>
+          <Card>
+            <TextField
+              label="Service Pressure(MPa)"
+              value={servicepressure1}
+              onChange={setForm}
+              name="servicepressure1"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+            />
+            <TextField
+              label="Manufactured Date"
+              value={cmanufacturedDate1}
+              onChange={setForm}
+              name="cmanufacturedDate1"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+            />
+            <TextField
+              label="Water volume(litres)"
+              value={waterVolume1}
+              onChange={setForm}
+              name="waterVolume1"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+            />
+            <TextField
+              label="Expiry Date"
+              value={expiryDate1}
+              onChange={setForm}
+              name="expiryDate1"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+            />
+            <TextField
+              label="TBS certificare"
+              value={tbscertificate1}
+              onChange={setForm}
+              name="tbscertificate1"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+            />
+          </Card>{" "}
+          <Card>
+            <TextField
+              label="Service Pressure(MPa)"
+              value={servicepressure2}
+              onChange={setForm}
+              name="servicepressure2"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+            />
+            <TextField
+              label="Manufactured Date"
+              value={cmanufacturedDate2}
+              onChange={setForm}
+              name="cmanufacturedDate2"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+            />
+            <TextField
+              label="Water volume(litres)"
+              value={waterVolume2}
+              onChange={setForm}
+              name="waterVolume2"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+            />
+            <TextField
+              label="Expiry Date"
+              value={expiryDate2}
+              onChange={setForm}
+              name="expiryDate2"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+            />
+            <TextField
+              label="TBS certificare"
+              value={tbscertificate2}
+              onChange={setForm}
+              name="tbscertificate2"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+            />
+          </Card>{" "}
+          <Card>
+            <TextField
+              label="Service Pressure(MPa)"
+              value={servicepressure3}
+              onChange={setForm}
+              name="servicepressure3"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+              fullWidth
+            />
+            <TextField
+              label="Manufactured Date"
+              value={cmanufacturedDate3}
+              onChange={setForm}
+              name="cmanufacturedDate3"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+              fullWidth
+            />
+            <TextField
+              label="Water volume(litres)"
+              value={waterVolume3}
+              onChange={setForm}
+              name="waterVolume3"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+              fullWidth
+            />
+            <TextField
+              label="Expiry Date"
+              value={expiryDate3}
+              onChange={setForm}
+              name="expiryDate3"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+              fullWidth
+            />
+            <TextField
+              label="TBS certificare"
+              value={tbscertificate3}
+              onChange={setForm}
+              name="tbscertificate3"
+              margin="normal"
+              variant="outlined"
+              autoComplete="off"
+              fullWidth
+            />
+          </Card>{" "}
+        </Card>{" "}
         <div style={{ marginTop: "1rem" }}>
           {" "}
           <Button
@@ -104,9 +213,9 @@ function CgnCylinderInfoCont({ formData, setForm, navigation }) {
             variant="contained"
             color="primary"
             style={{ marginRight: "1rem" }}
-            onClick={handle}
+            onClick={() => navigation.next()}
           >
-            Preview{" "}
+            Next{" "}
           </Button>{" "}
         </div>{" "}
       </Card>{" "}
